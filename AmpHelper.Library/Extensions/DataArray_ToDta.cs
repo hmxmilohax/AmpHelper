5c8c52e1d9631d125781ba4ec5686a4289a515b2
Daniel <790119+DanTheMan827@users.noreply.github.com>
2022-10-20 15:40:11 -0500

    ToDta

---
+++ AmpHelper.Library/Extensions/DataArray_ToDta.cs
@@ -0,0 +1,20 @@
+﻿using DtxCS.DataTypes;
+using System.Text;
+
+namespace AmpHelper.Library.Extensions
+{
+    public static class DataArray_ToDta
+    {
+        public static string ToDta(this DataArray array)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var child in array.Children)
+            {
+                sb.AppendLine(child.ToString(0));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
